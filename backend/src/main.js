c437be08029cd55077821b5aa522878c3f04bcda
benjamin.gangl@students.ffhs.ch <benjamin.gangl@students.ffhs.ch>
2024-06-01 15:46:55 +0200

    Implementing first scraping capabilities

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,30 +1,42 @@
-import { Client, Databases, ID } from 'node-appwrite';
+// import { Client, Databases, ID } from 'node-appwrite';
+import { Builder, Browser } from 'selenium-webdriver';
 
 export default async ({ res, log, error }) => {
+  const driver = await new Builder().forBrowser(Browser.CHROME).build();
+
+  await driver.get('https://www.grabenhalle.ch');
+
+  const title = await driver.getTitle();
+
+  log(title);
+
+  await driver.quit();
+
+  /*
   const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1')
     .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
     .setKey(process.env.APPWRITE_API_KEY);
 
-
   const databases = new Databases(client);
 
   try {
-    log('Hello, Logs!');
     await databases.createDocument(
       'usgang.sg',
       'events',
       ID.unique(),
-      { 
+      {
         "raw_html": "test",
         "club_name": "test",
         "event_description": "test",
-        "event_date": "07.05.2024, 15:27:05"
+        "event_date": "07.05.2024, 15:27:05",
+        "event_link": "https://www.digitec.ch"
       }
     )
   } catch (e) {
     error("Failed to create document: " + e.message)
   }
+  */
 
   return res.empty()
 };
