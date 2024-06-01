43a4eb7921c0195d9869f1f23025805651929534
benjamin.gangl@students.ffhs.ch <benjamin.gangl@students.ffhs.ch>
2024-06-01 16:52:09 +0200

    Download chrome driver locally

--- backend/src/scraping.js
+++ backend/src/scraping.js
@@ -2,9 +2,12 @@ import { Builder, Browser } from 'selenium-webdriver';
 import chrome from 'selenium-webdriver/chrome.js';
 
 export const scrape = async () => {
+    const options = new chrome.Options();
+    options.addArguments('--headless=new');
+
     const driver = await new Builder()
         .forBrowser(Browser.CHROME)
-        .setChromeOptions(new chrome.Options().addArguments('--headless=new'))
+        .setChromeOptions(options)
         .build();
 
     await driver.get('https://www.grabenhalle.ch');
