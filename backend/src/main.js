8e51c15c66f861ae92ef0b739523cfaa730f0bce
benjamin.gangl@students.ffhs.ch <benjamin.gangl@students.ffhs.ch>
2024-06-01 15:28:47 +0200

    Implementing the first database access

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,33 +1,28 @@
-import { Client } from 'node-appwrite';
+import { Client, Databases, ID } from 'node-appwrite';
 
-// This is your Appwrite function
-// It's executed each time we get a request
-export default async ({ req, res, log, error }) => {
-  // Why not try the Appwrite SDK?
-  //
-  // const client = new Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  //    .setKey(process.env.APPWRITE_API_KEY);
+export default async ({ log, error }) => {
+  const client = new Client()
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+    .setKey(process.env.APPWRITE_API_KEY);
 
-  // You can log messages to the console
-  log('Hello, Logs!');
 
-  // If something goes wrong, log an error
-  error('Hello, Errors!');
+  const databases = new Databases(client);
 
-  // The `req` object contains the request data
-  if (req.method === 'GET') {
-    // Send a response with the res object helpers
-    // `res.send()` dispatches a string back to the client
-    return res.send('Hello, World!');
+  try {
+    log('Hello, Logs!');
+    await databases.createDocument(
+      'usgang.sg',
+      'events',
+      ID.unique(),
+      { 
+        "raw_html": "test",
+        "club_name": "test",
+        "event_description": "test",
+        "event_date": "07.05.2024, 15:27:05"
+      }
+    )
+  } catch (e) {
+    error("Failed to create document: " + e.message)
   }
-
-  // `res.json()` is a handy helper for sending JSON
-  return res.json({
-    motto: 'Build like a team of hundreds_',
-    learn: 'https://appwrite.io/docs',
-    connect: 'https://appwrite.io/discord',
-    getInspired: 'https://builtwith.appwrite.io',
-  });
 };
