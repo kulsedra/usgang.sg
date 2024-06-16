cd96ca2b222e499fbf5db516fe024c1977c5a8b4
nico464 <nicolai4678@gmail.com>
2024-06-16 13:16:42 +0200

    frontend misc

--- frontend/src/app/day/day.component.ts
+++ frontend/src/app/day/day.component.ts
@@ -9,9 +9,9 @@ import { DbService } from '@app/heute/db.service';
 })
 export class DayComponent implements OnInit {
   public day: string;
-  events: any;
-  clubs: any;
-  selection: any;
+  events: any[] = [];
+  clubs: any[] = [];
+  selection: number;
   dayMapping: { [key: string]: number } = {
     'today': 0,
     'tomorrow': 1,
@@ -28,52 +28,44 @@ export class DayComponent implements OnInit {
   constructor(private route: ActivatedRoute, private db: DbService) { }
   
   ngOnInit(): void {
-    
     this.route.paramMap.subscribe(params => {
-
       this.day = params.get('day')!;
-      
-      this.selection = this.dayMapping[this.day] !== undefined ? this.dayMapping[this.day] : this.selection;
-      this.db.setSharedVariable(this.dayMappingString[this.day])
-
+      this.selection = this.dayMapping[this.day] ?? this.selection;
+      this.db.setSharedVariable(this.dayMappingString[this.day]);
       this.fetchClubs();
     });
-
-    
   }
 
-  async fetchEvents() {
+  async fetchEvents(): Promise<void> {
     try {
       const response = await this.db.getEvents(this.selection);
       this.events = response.documents; 
-      if(this.events){
-        this.clubs.forEach((club: any) => {
-          this.events.forEach((event: any) => {
-            if(club.$id === event.club.$id) {
-              club.event = event;
-            }
-          })
-        });
-      }
+
+      this.mapEventsToClubs();
     } catch (error) {
       console.error('Error fetching events:', error);
     }
-
-    
   }
 
-  async fetchClubs() {
+  async fetchClubs(): Promise<void> {
     try {
       const response = await this.db.getClubs();
       this.clubs = response.documents; 
-      this.fetchEvents();
+      await this.fetchEvents();
     } catch (error) {
-      console.error('Error fetching events:', error);
+      console.error('Error fetching clubs:', error);
     }
-}
+  }
 
+  private mapEventsToClubs(): void {
+    if (this.events.length > 0 && this.clubs.length > 0) {
+      this.clubs.forEach(club => {
+        club.event = this.events.find(event => event.club.$id === club.$id);
+      });
+    }
+  }
 
-  public open(club: any){
+  public open(club: any): void {
     window.location.href = club.event ? club.event.event_link : club.club_link;
   }
 }
